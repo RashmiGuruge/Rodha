7d0cda068f9efdf54457db9403a1d5566bbe9896
Rashmi Guruge <79540934+RashmiGuruge@users.noreply.github.com>
2022-07-15 10:04:00 +0530

    add

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,17 +1,17 @@
 var swiper = new Swiper(".testimonialSwiper", {
-    // loop: true,
-    slidesPerView: 1,
-    spaceBetween: 30,
-    grabCursor: true,
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
+  // loop: true,
+  slidesPerView: 1,
+  spaceBetween: 30,
+  grabCursor: true,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  breakpoints: {
+    // when window width is >= 992px
+    992: {
+      slidesPerView: 2,
+      spaceBetween: 0,
     },
-    breakpoints: {
-        // when window width is >= 992px
-        992: {
-          slidesPerView: 2,
-          spaceBetween: 0
-        }
-    }
-  });
+  },
+});
